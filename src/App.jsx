88eb941e3758c0d8caee4dc9e42e1c9e95ca812c
AlexF <fennecfr@gmail.com>
2024-07-11 19:01:17 +0200

    add store and todosSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTodo, removeTodo } from './redux/todosSlice';
+
 const App = () => {
-  
+  const [text, setText] = useState('');
+  const todos = useSelector(state => state.todos.todos);
+  const dispatch = useDispatch();
+
+  const handleAddTodo = () => {
+    if (text.trim()) {
+      dispatch(addTodo(text));
+      setText('');
+    }
+  };
+
+  const handleRemoveTodo = (id) => {
+    dispatch(removeTodo(id));
+  };
+
   return (
-    <></>
+    <div>
+      <h2>Lista de Tareas</h2>
+      <input 
+        type="text" 
+        value={text} 
+        onChange={(e) => setText(e.target.value)} 
+        placeholder="Escribe una tarea"
+      />
+      <button onClick={handleAddTodo}>Añadir</button>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => handleRemoveTodo(todo.id)}>Eliminar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
